f508d0896a527dbc979cf84c3ad77caa878f5061
josiasveras <josias_veras@hotmail.com>
2022-02-23 17:59:09 -0300

    Adicionando URL local e heroku

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -14,11 +14,19 @@ export class AuthService {
   ) { }
 
   entrar(userLogin: UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>('http://localhost:8080/usuario/logar', userLogin);
+    // URL local
+    // return this.http.post<UserLogin>('http://localhost:8080/usuario/logar', userLogin);
+
+    // URL heroku
+    return this.http.post<UserLogin>('https://blog-josias.herokuapp.com/usuario/logar', userLogin);
   }
 
   cadastrar(usuario: Usuario): Observable<Usuario>{
-    return this.http.post<Usuario>('http://localhost:8080/usuario/cadastrar', usuario);
+    // URL local
+    // return this.http.post<Usuario>('http://localhost:8080/usuario/cadastrar', usuario);
+
+    // URL Heroku
+    return this.http.post<Usuario>('https://blog-josias.herokuapp.com/usuario/cadastrar', usuario);
   }
 
 }
