af3d73aa2d10006edf2d6704f8cad0bd0100c596
josiasveras <josias_veras@hotmail.com>
2022-02-23 17:46:36 -0300

    Implementado lógica de logar e cadastrar usuário

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../models/Usuario';
+import { AuthService } from '../services/auth.service';
+
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario = new Usuario;
+  confirmarSenha: string;
+  tipoUser: string;
 
-  ngOnInit(): void {
+  constructor(private authService: AuthService, private router: Router) { 
+
   }
 
+  ngOnInit() {
+    window.scroll(0,0);
+  }
+
+  confirmaSenha(event: any) {
+    this.confirmarSenha = event.target.value;
+  }
+
+  tipoUsuario(event: any) {
+    this.tipoUser = event.target.value;
+  }
+
+  cadastrar() {
+    this.usuario.tipo = this.tipoUser;
+
+    if (this.usuario.senha != this.confirmarSenha) {
+      alert("A senha de ambos os campos devem ser iguais!");
+    } else {
+      this.authService.cadastrar(this.usuario).subscribe((resp: Usuario) => {
+        this.usuario = resp;
+        this.router.navigate(["/entrar"]);
+        alert("Usuário cadastrado com sucesso!");
+      });
+    }
+  }
 }
