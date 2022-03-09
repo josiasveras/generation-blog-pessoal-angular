eaafed2e268c20234e26a6e110f72ab473408e99
josiasveras <josias_veras@hotmail.com>
2022-03-09 00:04:14 -0300

    Atualizando e deletando tema

--- src/app/services/tema.service.ts
+++ src/app/services/tema.service.ts
@@ -26,12 +26,36 @@ export class TemaService {
     return this.http.get<Tema[]>('https://blog-josias.herokuapp.com/tema/buscar/tudo', this.token);
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+    // URL local
+    //return this.http.get<Tema>(`http://localhost:8080/tema/buscar/${id}`, this.token);
+
+    // URL Heroku
+    return this.http.get<Tema>(`https://blog-josias.herokuapp.com/tema/buscar/${id}`, this.token);
+  }
+
   postTema(tema: Tema): Observable<Tema>{
     // URL local
     //return this.http.post<Tema>('http://localhost:8080/tema/salvar', tema, this.token);
 
     // URL Heroku
     return this.http.post<Tema>('https://blog-josias.herokuapp.com/tema/salvar', tema, this.token);
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    // URL local
+    //return this.http.put<Tema>('http://localhost:8080/tema/atualizar', tema, this.token);
+
+    // URL Heroku
+    return this.http.put<Tema>('https://blog-josias.herokuapp.com/tema/atualizar', tema, this.token);
+  }
+
+  deleteTema(id: number){
+    // URL local
+    //return this.http.delete(`http://localhost:8080/tema/deletar/${id}`, this.token)
+
+    // URL Heroku
+    return this.http.delete(`https://blog-josias.herokuapp.com/tema/deletar/${id}`, this.token)
   }
   
 }
