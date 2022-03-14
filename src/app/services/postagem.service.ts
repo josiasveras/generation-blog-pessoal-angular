9f49f8ec072997e1a44959728ae8c9610e35eaef
josiasveras <josias_veras@hotmail.com>
2022-03-14 18:16:55 -0300

    PUT e Delete Postagem

--- src/app/services/postagem.service.ts
+++ src/app/services/postagem.service.ts
@@ -26,12 +26,36 @@ export class PostagemService {
     return this.http.get<Postagem[]>("https://blog-josias.herokuapp.com/postagem/buscar/tudo", this.token);
   }
 
+  getByIdPostagem(id: number): Observable<Postagem>{
+    // URL local
+    // return this.http.get<Postagem>(`http://localhost:8080/postagem/buscar/${id}`, this.token);
+
+    // URL Heroku
+    return this.http.get<Postagem>(`https://blog-josias.herokuapp.com/postagem/buscar/${id}`, this.token);
+  }
+
   postPostagem(postagem: Postagem): Observable<Postagem>{
     // URL local
     //return this.http.post<Postagem>("http://localhost:8080/postagem/salvar", postagem, this.token);
 
     // URL Heroku
     return this.http.post<Postagem>("https://blog-josias.herokuapp.com/postagem/salvar", postagem, this.token);
+  }
+
+  putPostagem(postagem: Postagem): Observable<Postagem>{
+    // URL local
+    // return this.http.put<Postagem>("http://localhost:8080/postagem/atualizar", postagem, this.token);
+
+    // URL Heroku
+    return this.http.put<Postagem>("https://blog-josias.herokuapp.com/postagem/atualizar", postagem, this.token);
+  }
+
+  deletePostagem(id: number){
+    // URL local
+    // return this.http.delete(`http://localhost:8080/postagem/deletar/${id}`, this.token);
+
+    // URL Heroku
+    return this.http.delete(`https://blog-josias.herokuapp.com/postagem/deletar/${id}`, this.token);
   }
 
 }
